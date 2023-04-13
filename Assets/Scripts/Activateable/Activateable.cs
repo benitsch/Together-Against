904f684292fa5b8c175e1cf2327059f3b30a1bd0
StefanHohnwald <stefan.hohnwald@gmail.com>
2023-04-13 23:58:41 +0200

    Added Activateable script base

---
+++ Assets/Scripts/Activateable/Activateable.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activateable : MonoBehaviour
+{
+    private int Counter = 0;
+    // Start is called before the first frame update
+    public void Activate()
+    {
+        this.Counter++;
+        if(this.Counter == 1)
+        {
+            this.Activate_Implementation();
+        }
+    }
+
+    protected virtual void Activate_Implementation()
+    {
+
+    }
+
+    public void Deactivate()
+    {
+        this.Counter--;
+        Debug.Assert(this.Counter >= 0);
+        if(this.Counter == 0)
+        {
+            this.Dectivate_Implementation();
+        }
+    }
+
+    protected virtual void Dectivate_Implementation()
+    {
+
+    }
+}
