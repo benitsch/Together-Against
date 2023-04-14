eba1ada999d525f4edc5e0380b95f8c39eebb67c
Prepe <37080825+Prepe@users.noreply.github.com>
2023-04-14 08:13:54 +0200

    Falling Platform Initital

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    [SerializeField] private bool FallOnExit = false;
+    [SerializeField] private float EnterTime = 6;
+    [SerializeField] private float ExitTime = 3;
+    [SerializeField] private float FallTime = 5;
+    [SerializeField] private float RespawnTime = 10;
+    [SerializeField, Layer] private int TriggerLayer = 0;
+    [SerializeField] private float mass = 10;
+    [SerializeField] private float drag = 10;
+
+    private bool triggerd = false;
+    private Vector3 position;
+
+    private void Start()
+    {
+        position = transform.position;
+        GetComponent<Rigidbody2D>().mass = mass;
+        GetComponent<Rigidbody2D>().drag = drag;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (FallOnExit || triggerd) return;
+        if (collision.collider.gameObject.layer != TriggerLayer) return;
+        Debug.Log(collision.transform.tag);
+
+        triggerd = true;
+        Invoke("StartFalling", EnterTime);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!FallOnExit || triggerd) return;
+        if (collision.collider.gameObject.layer != TriggerLayer) return;
+
+        triggerd = true;
+        Invoke("StartFalling", ExitTime);
+    }
+
+    void StartFalling()
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb.bodyType == RigidbodyType2D.Kinematic) rb.bodyType = RigidbodyType2D.Dynamic;
+        Invoke("GoInactive", FallTime);
+    }
+
+    void GoInactive()
+    {
+        Invoke("Respawn", RespawnTime);
+        gameObject.SetActive(false);
+    }
+
+    void Respawn()
+    {
+        GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+        transform.position = position;
+        triggerd = false;
+        gameObject.SetActive(true);
+    }
+}
