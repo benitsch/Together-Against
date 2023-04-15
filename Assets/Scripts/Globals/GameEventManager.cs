0ea4f5e4eed742d33bc9f00e85a8a85eef9bf583
StefanHohnwald <stefan.hohnwald@gmail.com>
2023-04-15 10:52:43 +0200

    Update GameEventManager.cs

--- Assets/Scripts/Globals/GameEventManager.cs
+++ Assets/Scripts/Globals/GameEventManager.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerScoreReason
+{
+    None,
+    OffScreen,
+    HazardDeath,
+    Conveyor,
+    EndLevelTimer
+}
 public delegate void OnPlayerEventDelegate(int playerID);
 public class GameEventManager : Singleton<GameEventManager>
 {
+
+
     private static GameEventManager instance;
 
     OnPlayerEventDelegate OnPlayerDeath;
     OnPlayerEventDelegate OnPlayerReachedFinish;
 
-    public void PlayerDied(int playerID)
+    public int Player1Score = 0;
+    public int Player2Score = 0;
+
+    public void PlayerScoreEvent(int playerID, PlayerScoreReason scoreReason)
     {
         OnPlayerDeath?.Invoke(playerID);
     }
