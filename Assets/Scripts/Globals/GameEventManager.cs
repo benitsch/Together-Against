f5789870cd86b224eaa8e0f995d6b4fbdc008b0a
StefanHohnwald <stefan.hohnwald@gmail.com>
2023-04-15 11:48:06 +0200

    Update GameEventManager.cs

--- Assets/Scripts/Globals/GameEventManager.cs
+++ Assets/Scripts/Globals/GameEventManager.cs
@@ -8,24 +8,90 @@ public enum PlayerScoreReason
     OffScreen,
     HazardDeath,
     Conveyor,
-    EndLevelTimer
+    ReachedFinish
 }
 public delegate void OnPlayerEventDelegate(int playerID);
+public delegate void OnGenericEventDelegate();
 public class GameEventManager : Singleton<GameEventManager>
 {
 
 
     private static GameEventManager instance;
 
-    OnPlayerEventDelegate OnPlayerDeath;
-    OnPlayerEventDelegate OnPlayerReachedFinish;
+    public OnPlayerEventDelegate OnPlayerScoreEvent;
+    public OnPlayerEventDelegate OnPlayerReachedFinish;
+    public OnGenericEventDelegate OnLevelTimeEnded;
 
+    public bool Player1ReachedFinish = false;
+    public bool Player2ReachedFinish = false;
     public int Player1Score = 0;
     public int Player2Score = 0;
 
+    public void LevelTimeEnded()
+    {
+        OnLevelTimeEnded?.Invoke();
+        ClearDelegates();
+    }
+
+    private void ClearDelegates()
+    {
+        OnLevelTimeEnded = null;
+        OnPlayerScoreEvent = null;
+        OnPlayerReachedFinish = null;
+    }
     public void PlayerScoreEvent(int playerID, PlayerScoreReason scoreReason)
     {
-        OnPlayerDeath?.Invoke(playerID);
+        int targetPlayerID = 0;
+        switch (scoreReason)
+        {
+            case PlayerScoreReason.None:
+                break;
+            case PlayerScoreReason.OffScreen:
+                targetPlayerID = 1 - playerID;
+                if (playerID == 0)
+                {
+                    Player1Score = Player1Score + 1;
+                }
+                else
+                {
+                    Player2Score = Player2Score + 1;
+                }
+                break;
+            case PlayerScoreReason.HazardDeath:
+                targetPlayerID = 1 - playerID;
+                if(playerID == 0)
+                {
+                    Player1Score = Player1Score + 1;
+                }
+                else
+                {
+                    Player2Score = Player2Score + 1;
+                }
+                break;
+            case PlayerScoreReason.Conveyor:
+                targetPlayerID = 1 - playerID;
+                if (playerID == 0)
+                {
+                    Player1Score = Player1Score + 1;
+                }
+                else
+                {
+                    Player2Score = Player2Score + 1;
+                }
+                break;
+            case PlayerScoreReason.EndLevelTimer:
+                targetPlayerID = 1 - playerID;
+                if (playerID == 0)
+                {
+                    Player1Score = Player1Score + 1;
+                }
+                else
+                {
+                    Player2Score = Player2Score + 1;
+                }
+                break;
+        }
+        OnPlayerScoreEvent?.Invoke(playerID);
     }
 
     public void PlayerReachedFinish(int playerID)
